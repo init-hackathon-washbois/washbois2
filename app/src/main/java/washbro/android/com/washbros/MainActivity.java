d6700aedbbeaf2d8a68011ed32d84eca698f5c95
paenugula <paenugul@calpoly.edu>
2018-11-10 17:33:57 -0800

    Some Firebase code

--- app/src/main/java/washbro/android/com/washbros/MainActivity.java
+++ app/src/main/java/washbro/android/com/washbros/MainActivity.java
@@ -2,19 +2,22 @@ package washbro.android.com.washbros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnToRoom, btnToCreate;
     private EditText editTextRoomId;
     private FirebaseAuth mAuth;
-    DatabaseReference mJobs;
+    DatabaseReference mRooms;
     DatabaseReference mUsers;
 
 
@@ -26,7 +29,18 @@ public class MainActivity extends AppCompatActivity {
         btnToRoom = findViewById(R.id.btnGoToRoom);
         btnToCreate = findViewById(R.id.btnGoToCreate);
         editTextRoomId = findViewById(R.id.editTextRoomId);
-        
+        mRooms = FirebaseDatabase.getInstance().getReference("Rooms");
+        mUsers = FirebaseDatabase.getInstance().getReference("Users");
+        final String roomID =editTextRoomId.getText().toString();
+        btnToRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(roomID.equals("")){
+                    Toast.makeText(MainActivity.this, "Enter a Room ID", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
 
 
 
