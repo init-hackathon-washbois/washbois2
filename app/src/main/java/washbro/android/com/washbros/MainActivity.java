90243904b216117827bf65166804c0da79a7b774
paenugula <paenugul@calpoly.edu>
2018-11-10 16:01:31 -0800

    literally restarted new layouts and activities added

--- app/src/main/java/washbro/android/com/washbros/MainActivity.java
+++ app/src/main/java/washbro/android/com/washbros/MainActivity.java
@@ -2,12 +2,34 @@ package washbro.android.com.washbros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnToRoom, btnToCreate;
+    private EditText editTextRoomId;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnToRoom = findViewById(R.id.btnGoToRoom);
+        btnToCreate = findViewById(R.id.btnGoToCreate);
+        editTextRoomId = findViewById(R.id.editTextRoomId);
+
+
+
+    }
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 }
